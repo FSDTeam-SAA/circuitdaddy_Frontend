c4097b9d745ab5e97763f7481eeef59e6d0374b2
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2025-11-22 15:41:26 +0600

    states done

--- src/app/(engineer)/_components/state-card.tsx
+++ src/app/(engineer)/_components/state-card.tsx
@@ -12,7 +12,7 @@ const StateCard = ({
   image: string;
 }) => {
   return (
-    <div className="bg-white p-5 rounded-lg shadow-sm border border-gray-200 flex items-center justify-between h-[122px]">
+    <div className="bg-white p-5 rounded-lg shadow-sm border border-gray-200 flex items-center justify-between h-[130px]">
       <div>
         <h4 className="text-sm text-gray-500">{title}</h4>
         <h4 className="font-medium text-2xl mt-2">{value}</h4>
